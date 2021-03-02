ae597d288fb3dbb744131d903f301a21b847a76e
John3den <64809077+John3den@users.noreply.github.com>
2021-03-02 17:00:11 +0700

    deleted stuff

---
+++ BinarySearch/source code/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ll long long
+#define max(X, Y)  ((X) >= (Y) ? (X) : (Y))
+#define min(X, Y)  ((X) <= (Y) ? (X) : (Y))
+
+#include <stdio.h>
+
+int main() {
+
+	return 0;
+}
